791e12c42eaaaf804ffe541ebe982befb335d0ae
Arnoochka <kolos.130278@gmail.com>
2024-12-21 23:05:54 +0300

    get pipeline results

--- src/net.py
+++ src/net.py
@@ -2,26 +2,49 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-class ResNet18(nn.Module):
+class ImprovedCNN(nn.Module):
     def __init__(self, num_classes):
-        super(ResNet18, self).__init__()
-        self.resnet = models.resnet18(pretrained=True)
-        
-        for param in self.resnet.parameters():
-            param.requires_grad = False
-
-        self.resnet.fc = nn.Sequential(
-            nn.Linear(self.resnet.fc.in_features, 512),
+        super(ImprovedCNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(512, num_classes),
-            nn.ReLU(),
-            nn.Softmax(dim=1)
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
-
-        # Размораживаем параметры только для fc
-        for param in self.resnet.fc.parameters():
-            param.requires_grad = True
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=1)
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1))  # Глобальный пул
+        )
+        self.fc1 = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.Dropout(0.4),  # Увеличенный Dropout
+            nn.ReLU()
+        )
+        self.fc2 = nn.Linear(128, num_classes)
+        self.softmax = nn.Softmax()
+        
 
     def forward(self, x):
-        return self.resnet(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.softmax(x)
+        return x
--- src/train.py
+++ src/train.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import pickle
 import torch
-from net import ResNet18 as Net
+from net import ImprovedCNN as Net
 from tqdm import tqdm
 import os
 import yaml
